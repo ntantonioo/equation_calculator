e587c103e94d0a1ed52d48dbdc99677922b482b9
ntantonioo <antonioribeiroan7376914@gmail.com>
2026-09-20 22:18:35 -0300

    Implementação da classe Main, que faz a montagem de todas as informações e dá o "start" no programa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import operacao.aritmetica.Divisao;
+import operacao.aritmetica.Soma;
+import service.CalculadoraService;
+import service.CatalogoOperacoes;
+import ui.ConsoleUI;
+
 public class Main {
+
+    public static void main(String[] args) {
+        CatalogoOperacoes catalogo = new CatalogoOperacoes();
+
+        catalogo.registrar(new Soma());
+        catalogo.registrar(new Divisao());
+
+        CalculadoraService service = new CalculadoraService(catalogo);
+        ConsoleUI ui = new ConsoleUI(service);
+        ui.iniciar();
+    }
 }
