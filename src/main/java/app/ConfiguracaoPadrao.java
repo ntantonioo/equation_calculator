e475355fceef4d128578cfd5cf82124ca3c7f1c8
ntantonioo <antonioribeiroan7376914@gmail.com>
2026-09-24 10:46:44 -0300

    Implementação da classe ConfiguracaoPadrao, que monta o catalogo de todas as operações disponiveis, para unificar o console e interface gráfica

---
+++ src/main/java/app/ConfiguracaoPadrao.java
@@ -0,0 +1,28 @@
+package app;
+
+import operacao.aritmetica.Divisao;
+import operacao.aritmetica.Soma;
+import operacao.aritmetica.Multiplicacao;
+import operacao.aritmetica.Subtracao;
+import operacao.geometria.AreaCirculo;
+import operacao.geometria.AreaTriangulo;
+import operacao.segundoGrau.EquacaoSegundoGrau;
+import service.CalculadoraService;
+import service.CatalogoOperacoes;
+
+public class ConfiguracaoPadrao {
+
+    public static CalculadoraService montarService() {
+        CatalogoOperacoes catalogoOperacoes = new CatalogoOperacoes();
+
+        catalogoOperacoes.registrar(new Soma());
+        catalogoOperacoes.registrar(new Divisao());
+        catalogoOperacoes.registrar(new Multiplicacao());
+        catalogoOperacoes.registrar(new Subtracao());
+        catalogoOperacoes.registrar(new EquacaoSegundoGrau());
+        catalogoOperacoes.registrar(new AreaCirculo());
+        catalogoOperacoes.registrar(new AreaTriangulo());
+
+        return new CalculadoraService(catalogoOperacoes);
+    }
+}
